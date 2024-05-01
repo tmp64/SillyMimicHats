bf9bced570d4995cdcdf7a2e7d09ba20ad554f5e
tmp64 <tmp32x2@gmail.com>
2024-05-01 17:54:41 +0700

    Update ModMain to match the example

--- SillyMimicHats/ModMain.cpp
+++ SillyMimicHats/ModMain.cpp
@@ -7,155 +7,191 @@
 
 ModMain* gMod = nullptr;
 
+//---------------------------------------------------------------------------------
+// Function Hooks
+//---------------------------------------------------------------------------------
 auto g_CArkNpcSpawner_GetEntityArchetype_Hook = CArkNpcSpawner::FGetEntityArchetype.MakeHook();
 
 IEntityArchetype* GetArchetypeOverride(const CArkNpcSpawner* const _this)
 {
-	SmartScriptTable pScript = _this->GetEntity()->GetScriptTable();
-	if (!pScript)
-		return nullptr;
+    SmartScriptTable pScript = _this->GetEntity()->GetScriptTable();
+    if (!pScript)
+        return nullptr;
 
-	ArkSafeScriptTable pScriptSafe = pScript;
-	ArkSafeScriptTable pProps = pScriptSafe.GetSafeSubTable("Properties");
-	if (!pProps.m_pScriptTable)
-		return nullptr;
+    ArkSafeScriptTable pScriptSafe = pScript;
+    ArkSafeScriptTable pProps = pScriptSafe.GetSafeSubTable("Properties");
+    if (!pProps.m_pScriptTable)
+        return nullptr;
 
-	const char* archetypeName = pProps.GetString("sNpcArchetype", nullptr);
-	if (!archetypeName)
-		return nullptr;
+    const char* archetypeName = pProps.GetString("sNpcArchetype", nullptr);
+    if (!archetypeName)
+        return nullptr;
 
-	const std::string* pNewName = gMod->GetReplacementArchetype(archetypeName);
-	if (!pNewName)
-		return nullptr;
+    const std::string* pNewName = gMod->GetReplacementArchetype(archetypeName);
+    if (!pNewName)
+        return nullptr;
 
-	IEntityArchetype* pArchetype = gEnv->pEntitySystem->LoadEntityArchetype(pNewName->c_str());
-	if (!pArchetype)
-	{
-		CryError("Archetype '{}' not found", *pNewName);
-		return nullptr;
-	}
+    IEntityArchetype* pArchetype = gEnv->pEntitySystem->LoadEntityArchetype(pNewName->c_str());
+    if (!pArchetype)
+    {
+        CryError("Archetype '{}' not found", *pNewName);
+        return nullptr;
+    }
 
-	return pArchetype;
+    return pArchetype;
 }
 
 IEntityArchetype* CArkNpcSpawner_GetEntityArchetype_Hook(const CArkNpcSpawner* const _this)
 {
-	IEntityArchetype* pOverride = GetArchetypeOverride(_this);
+    IEntityArchetype* pOverride = GetArchetypeOverride(_this);
 
-	if (pOverride)
-		return pOverride;
+    if (pOverride)
+        return pOverride;
 
-	return g_CArkNpcSpawner_GetEntityArchetype_Hook.InvokeOrig(_this);;
+    return g_CArkNpcSpawner_GetEntityArchetype_Hook.InvokeOrig(_this);;
 }
 
-void ModMain::FillModInfo(ModDllInfoEx& info)
-{
-	info.modName = "tmp64.SillyMimicHats";
-	info.logTag = "SillyMimicHats";
-	info.supportsHotReload = true;
-}
-
-void ModMain::InitHooks()
-{
-	g_CArkNpcSpawner_GetEntityArchetype_Hook.SetHookFunc(&CArkNpcSpawner_GetEntityArchetype_Hook);
-}
-
-void ModMain::InitSystem(const ModInitInfo& initInfo, ModDllInfo& dllInfo)
-{
-	BaseClass::InitSystem(initInfo, dllInfo);
-    ChairSetGlobalModName("tmp64.SillyMimicHats");
-
-	m_Rng = std::mt19937(gEnv->bNoRandomSeed ? 0u : std::random_device()());
-
-	// Default archetypes
-	const std::string MIMIC = "ArkNpcs.Mimics.Mimic";
-	RegisterArchetype(MIMIC, MIMIC + ".BucketHat");
-	RegisterArchetype(MIMIC, MIMIC + ".CrownHat");
-	RegisterArchetype(MIMIC, MIMIC + ".RiceHat");
-	RegisterArchetype(MIMIC, MIMIC + ".Sombrero");
-	RegisterArchetype(MIMIC, MIMIC + ".StripedHat");
-	RegisterArchetype(MIMIC, MIMIC + ".TopHat");
-	RegisterArchetype(MIMIC, MIMIC + ".WitchHat");
-}
-
-void ModMain::InitGame(bool isHotReloading)
-{
-	BaseClass::InitGame(isHotReloading);
-	// Your code goes here
-}
-
-void ModMain::ShutdownGame(bool isHotUnloading)
-{
-	// Your code goes here
-	BaseClass::ShutdownGame(isHotUnloading);
-}
-
-void ModMain::ShutdownSystem(bool isHotUnloading)
-{
-	// Your code goes here
-	BaseClass::ShutdownSystem(isHotUnloading);
-}
-
-void ModMain::Draw()
-{
-}
-
-void ModMain::MainUpdate(unsigned updateFlags)
-{
-	// Your code goes here
-}
-
-
-
-void *ModMain::QueryInterface(const char *ifaceName) {
-    return nullptr;
-}
-
-void ModMain::Connect(const std::vector<IChairloaderMod *> &mods) {
-}
-
+//---------------------------------------------------------------------------------
+// Mod Methods
+//---------------------------------------------------------------------------------
 const std::string* ModMain::GetReplacementArchetype(std::string_view originalArchetype)
 {
-	auto it = m_ArchetypeMap.find(originalArchetype);
+    auto it = m_ArchetypeMap.find(originalArchetype);
 
-	if (it == m_ArchetypeMap.end())
-		return nullptr;
+    if (it == m_ArchetypeMap.end())
+        return nullptr;
 
-	const std::vector<std::string>& list = it->second;
-	CRY_ASSERT(!list.empty());
-	std::uniform_int_distribution<int> distribution(0, (int)list.size() - 1);
-	int idx = distribution(m_Rng);
-	return &list[idx];
+    const std::vector<std::string>& list = it->second;
+    CRY_ASSERT(!list.empty());
+    std::uniform_int_distribution<int> distribution(0, (int)list.size() - 1);
+    int idx = distribution(m_Rng);
+    return &list[idx];
 }
 
 void ModMain::RegisterArchetype(std::string_view originalName, std::string_view newName)
 {
-	auto it = m_ArchetypeMap.find(originalName);
+    auto it = m_ArchetypeMap.find(originalName);
 
-	if (it != m_ArchetypeMap.end())
-	{
-		// Append to the exsting list
-		it->second.push_back(std::string(newName));
-	}
-	else
-	{
-		// Create a new list
-		m_ArchetypeMap.emplace(std::string(originalName), std::vector<std::string> { std::string(newName) });
-	}
+    if (it != m_ArchetypeMap.end())
+    {
+        // Append to the exsting list
+        it->second.push_back(std::string(newName));
+    }
+    else
+    {
+        // Create a new list
+        m_ArchetypeMap.emplace(std::string(originalName), std::vector<std::string> { std::string(newName) });
+    }
 }
 
+//---------------------------------------------------------------------------------
+// Mod Initialization
+//---------------------------------------------------------------------------------
+void ModMain::FillModInfo(ModDllInfoEx& info)
+{
+    info.modName = "tmp64.SillyMimicHats";
+    info.logTag = "SillyMimicHats";
+    info.supportsHotReload = true;
+}
+
+void ModMain::InitHooks()
+{
+    g_CArkNpcSpawner_GetEntityArchetype_Hook.SetHookFunc(&CArkNpcSpawner_GetEntityArchetype_Hook);
+}
+
+void ModMain::InitSystem(const ModInitInfo& initInfo, ModDllInfo& dllInfo)
+{
+    BaseClass::InitSystem(initInfo, dllInfo);
+
+    m_Rng = std::mt19937(gEnv->bNoRandomSeed ? 0u : std::random_device()());
+
+    // Default archetypes
+    const std::string MIMIC = "ArkNpcs.Mimics.Mimic";
+    RegisterArchetype(MIMIC, MIMIC + ".BucketHat");
+    RegisterArchetype(MIMIC, MIMIC + ".CrownHat");
+    RegisterArchetype(MIMIC, MIMIC + ".RiceHat");
+    RegisterArchetype(MIMIC, MIMIC + ".Sombrero");
+    RegisterArchetype(MIMIC, MIMIC + ".StripedHat");
+    RegisterArchetype(MIMIC, MIMIC + ".TopHat");
+    RegisterArchetype(MIMIC, MIMIC + ".WitchHat");
+}
+
+void ModMain::InitGame(bool isHotReloading)
+{
+    BaseClass::InitGame(isHotReloading);
+    // Your code goes here
+}
+
+//---------------------------------------------------------------------------------
+// Mod Shutdown
+//---------------------------------------------------------------------------------
+void ModMain::ShutdownGame(bool isHotUnloading)
+{
+    // Your code goes here
+    BaseClass::ShutdownGame(isHotUnloading);
+}
+
+void ModMain::ShutdownSystem(bool isHotUnloading)
+{
+    // Your code goes here
+    BaseClass::ShutdownSystem(isHotUnloading);
+}
+
+//---------------------------------------------------------------------------------
+// GUI
+//---------------------------------------------------------------------------------
+void ModMain::Draw()
+{
+}
+
+//---------------------------------------------------------------------------------
+// Main Update Loop
+//---------------------------------------------------------------------------------
+void ModMain::MainUpdate(unsigned updateFlags)
+{
+    // Your code goes here
+}
+
+//---------------------------------------------------------------------------------
+// Mod Interfacing
+//---------------------------------------------------------------------------------
+#ifdef EXAMPLE
+void* ModMain::QueryInterface(const char* ifaceName)
+{
+    // This function is called by other mods to get interfaces.
+    // Check if you implement the interface. If so, return a pointer to it.
+    // This example assumes ModMain inherits from the interface class. You may choose a different object if you want.
+    if (!strcmp(ifaceName, IModName::IFACE_NAME))
+        return static_cast<IModName*>(this);
+
+    return nullptr;
+}
+
+void ModMain::Connect(const std::vector<IChairloaderMod*>& mods)
+{
+    IModName* pModName = nullptr; // Add this to ModMain
+
+    // You can use one of these helper functions. They will handle finding and version checking.
+    pModName = GetInterfaceFromModList<IModName>(mods, "Other Mod Name");
+    pModName = GetRequiredInterfaceFromModList<IModName>(mods, "Other Mod Name");
+}
+#endif
+
+//---------------------------------------------------------------------------------
+// Exported Functions
+//---------------------------------------------------------------------------------
 extern "C" DLL_EXPORT IChairloaderMod* ClMod_Initialize()
 {
-	CRY_ASSERT(!gMod);
-	gMod = new ModMain();
-	return gMod;
+    CRY_ASSERT(!gMod);
+    gMod = new ModMain();
+    return gMod;
 }
 
 extern "C" DLL_EXPORT void ClMod_Shutdown()
 {
-	CRY_ASSERT(gMod);
-	delete gMod;
-	gMod = nullptr;
+    CRY_ASSERT(gMod);
+    delete gMod;
+    gMod = nullptr;
 }
 
 // Validate that declarations haven't changed
--- SillyMimicHats/ModMain.h
+++ SillyMimicHats/ModMain.h
@@ -2,72 +2,86 @@
 #include <random>
 #include <ChairLoader/ModSDK/ChairloaderModBase.h>
 
-class ModMain : public ChairloaderModBase
+class ModMain final : public ChairloaderModBase
 {
-	using BaseClass = ChairloaderModBase;
+public:
+    using BaseClass = ChairloaderModBase;
 
-	//! Fills in the DLL info during initialization.
-	virtual void FillModInfo(ModDllInfoEx& info) override;
+    //---------------------------------------------------------------------------------
+    // Mod Methods
+    //---------------------------------------------------------------------------------
 
+    //! Returns a new archetype name for a given original archetype.
+    //! Modifies internal RNG state.
+    //! @param	originalArchetype	Original archetype name.
+    //! @returns New archetype name of nullptr if not replaced.
+    const std::string* GetReplacementArchetype(std::string_view originalArchetype);
+
+    //! Registers a new archetype for replacement.
+    void RegisterArchetype(std::string_view originalName, std::string_view newName);
+
+    //---------------------------------------------------------------------------------
+    // Mod Initialization
+    //---------------------------------------------------------------------------------
+    //! Fills in the DLL info during initialization.
+    virtual void FillModInfo(ModDllInfoEx& info) override;
+
+    //! Initializes function hooks before they are installed.
+    virtual void InitHooks() override;
+
+    //! Called during CSystem::Init, before any engine modules.
+    virtual void InitSystem(const ModInitInfo& initInfo, ModDllInfo& dllInfo) override;
+
+    //! Called after CGame::Init
+    virtual void InitGame(bool isHotReloading) override;
+
+    //---------------------------------------------------------------------------------
+    // Mod Shutdown
+    //---------------------------------------------------------------------------------
+    //! Called before CGame::Shutdown.
+    virtual void ShutdownGame(bool isHotUnloading) override;
+
+    //! Called before CSystem::Shutdown.
+    virtual void ShutdownSystem(bool isHotUnloading) override;
+
+    //---------------------------------------------------------------------------------
+    // GUI
+    //---------------------------------------------------------------------------------
+    //! Called just before MainUpdate to draw GUI. Only called when GUI is visible.
+    virtual void Draw() override;
+
+    //---------------------------------------------------------------------------------
+    // Main Update Loop
+    //---------------------------------------------------------------------------------
+    //! Earliest point of update in a frame, before CGame::Update. The timer still tracks time for the previous frame.
+    virtual void UpdateBeforeSystem(unsigned updateFlags) override {}
+
+    //! Called before physics is updated for the new frame, best point for queuing physics jobs.
+    //! This is like FixedUpdate() in Unity (but not FPS-independent). Use gEnv->pTimer->GetFrameTime() for time delta.
+    virtual void UpdateBeforePhysics(unsigned updateFlags) override {}
+
+    //! Called after entities have been updated but before FlowSystem and FlashUI.
+    //! This is the main update where most game logic is expected to occur.
+    //! Should be preferred if you don't need any special behavior.
+    virtual void MainUpdate(unsigned updateFlags) override;
+
+    //! Called after most of game logic has been updated, before CCryAction::PostUpdate.
+    virtual void LateUpdate(unsigned updateFlags) override {}
+
+    //---------------------------------------------------------------------------------
+    // Mod Interfacing
+    //---------------------------------------------------------------------------------
     //! Retrieves an interface for the mod.
-    void* QueryInterface(const char *ifaceName) override;
-
-	//! Initializes function hooks before they are installed.
-	virtual void InitHooks() override;
-
-	//! Called during CSystem::Init, before any engine modules.
-	//! Call order: TODO
-	virtual void InitSystem(const ModInitInfo& initInfo, ModDllInfo& dllInfo) override;
+    // virtual void* QueryInterface(const char *ifaceName) override;
 
     //! Called after CSystem::Init, after all engine modules and mods have been initialized. Allows your mod to get interfaces from other mods.
-    void Connect(const std::vector<IChairloaderMod *> &mods) override;
+    // virtual void Connect(const std::vector<IChairloaderMod*>& mods) override;
 
 private:
-    //! Called after CGame::Init
-	//! Call order: TODO
-	virtual void InitGame(bool isHotReloading) override;
+    std::mt19937 m_Rng;
 
-	//! Called before CGame::Shutdown.
-	//! Call order: TODO
-	virtual void ShutdownGame(bool isHotUnloading) override;
-
-	//! Called before CSystem::Shutdown.
-	//! Call order: TODO
-	virtual void ShutdownSystem(bool isHotUnloading) override;
-
-	//! Called just before MainUpdate to draw GUI. Only called when GUI is visible.
-	virtual void Draw() override;
-
-	//! Earliest point of update in a frame, before CGame::Update. The timer still tracks time for the previous frame.
-	// virtual void UpdateBeforeSystem(unsigned updateFlags) override;
-
-	//! Called before physics is updated for the new frame, best point for queuing physics jobs.
-	//! This is like FixedUpdate() in Unity (but not FPS-independent). Use gEnv->pTimer->GetFrameTime() for time delta.
-	// virtual void UpdateBeforePhysics(unsigned updateFlags) override;
-
-	//! Called after entities have been updated but before FlowSystem and FlashUI.
-	//! This is the main update where most game logic is expected to occur.
-	//! Should be preferred if you don't need any special behavior.
-	virtual void MainUpdate(unsigned updateFlags) override;
-
-	//! Called after most of game logic has been updated, before CCryAction::PostUpdate.
-	// virtual void LateUpdate(unsigned updateFlags) override;
-
-public:
-	//! Returns a new archetype name for a given original archetype.
-	//! Modifies internal RNG state.
-	//! @param	originalArchetype	Original archetype name.
-	//! @returns New archetype name of nullptr if not replaced.
-	const std::string* GetReplacementArchetype(std::string_view originalArchetype);
-
-	//! Registers a new archetype for replacement.
-	void RegisterArchetype(std::string_view originalName, std::string_view newName);
-
-private:
-	std::mt19937 m_Rng;
-
-	//! Maps archetype name to a list of archetypes with hats.
-	std::map<std::string, std::vector<std::string>, std::less<>> m_ArchetypeMap;
+    //! Maps archetype name to a list of archetypes with hats.
+    std::map<std::string, std::vector<std::string>, std::less<>> m_ArchetypeMap;
 };
 
 extern ModMain* gMod;
